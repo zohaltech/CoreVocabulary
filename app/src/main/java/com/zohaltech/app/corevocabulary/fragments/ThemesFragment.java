a7b76e8763a2bc0f8a192346831a970ee7764fff
ostademast <ostademast@yahoo.com>
2015-09-12 16:17:27 +0430

    vocabularies ui

--- app/src/main/java/com/zohaltech/app/corevocabulary/fragments/ThemesFragment.java
+++ app/src/main/java/com/zohaltech/app/corevocabulary/fragments/ThemesFragment.java
@@ -3,10 +3,11 @@ package com.zohaltech.app.corevocabulary.fragments;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.zohaltech.app.corevocabulary.R;
 import com.zohaltech.app.corevocabulary.adapters.ThemeAdapter;
@@ -18,9 +19,10 @@ import java.util.ArrayList;
 
 public class ThemesFragment extends Fragment {
 
-    ListView         lstPackagesHistories;
+    RecyclerView     recyclerThemes;
     ArrayList<Theme> themes;
     ThemeAdapter     adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     public ThemesFragment() {
     }
@@ -34,12 +36,13 @@ public class ThemesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_themes, container, false);
-
-        lstPackagesHistories = (ListView) rootView.findViewById(R.id.lstThemes);
+        recyclerThemes = (RecyclerView) rootView.findViewById(R.id.recyclerThemes);
+        recyclerThemes.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerThemes.setLayoutManager(layoutManager);
         themes = Themes.select();
-        adapter = new ThemeAdapter(themes);
-        lstPackagesHistories.setAdapter(adapter);
-
+        adapter = new ThemeAdapter(getActivity(), themes);
+        recyclerThemes.setAdapter(adapter);
         return rootView;
     }
 
