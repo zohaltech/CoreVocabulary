849a25c77cb48a3ae3009a258d3405d5380d181e
ali <ali.faghani@gmail.com>
2015-09-13 10:12:14 +0430

    add search activity, layout and  adapter

---
+++ app/src/main/java/com/zohaltech/app/corevocabulary/activities/SearchActivity.java
@@ -0,0 +1,49 @@
+package com.zohaltech.app.corevocabulary.activities;
+
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+
+import com.zohaltech.app.corevocabulary.R;
+import com.zohaltech.app.corevocabulary.adapters.VocabularyAdapter;
+import com.zohaltech.app.corevocabulary.data.Vocabularies;
+import com.zohaltech.app.corevocabulary.entities.Theme;
+import com.zohaltech.app.corevocabulary.entities.Vocabulary;
+
+import java.util.ArrayList;
+
+public class SearchActivity extends EnhancedActivity {
+
+    private Theme theme;
+
+    @Override
+    void onCreated() {
+        setContentView(R.layout.activity_search);
+        RecyclerView recyclerVocabularies = (RecyclerView) findViewById(R.id.recyclerVocabularies);
+        recyclerVocabularies.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerVocabularies.setLayoutManager(layoutManager);
+        theme = (Theme) getIntent().getSerializableExtra("THEME");
+        ArrayList<Vocabulary> vocabularies = Vocabularies.selectByTheme(theme.getId());
+        VocabularyAdapter adapter = new VocabularyAdapter(this, vocabularies);
+        recyclerVocabularies.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    void onToolbarCreated() {
+        //txtToolbarTitle.setText("Vocabularies");
+        getSupportActionBar().setTitle(theme.getName());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+}
---
+++ app/src/main/java/com/zohaltech/app/corevocabulary/adapters/SearchResultAdapter.java
@@ -0,0 +1,62 @@
+package com.zohaltech.app.corevocabulary.adapters;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.zohaltech.app.corevocabulary.R;
+import com.zohaltech.app.corevocabulary.activities.VocabDescriptionActivity;
+import com.zohaltech.app.corevocabulary.classes.App;
+import com.zohaltech.app.corevocabulary.entities.Vocabulary;
+
+import java.util.ArrayList;
+
+public class SearchResultAdapter extends RecyclerView.Adapter<SearchResultAdapter.ViewHolder> {
+    Context               context;
+    ArrayList<Vocabulary> vocabularies;
+
+    public SearchResultAdapter(Context context, ArrayList<Vocabulary> vocabularies) {
+        this.context = context;
+        this.vocabularies = vocabularies;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.adapter_search_result, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final Vocabulary vocabulary = vocabularies.get(position);
+
+        holder.txtSearchResult.setText(vocabulary.getVocabulary());
+        holder.txtSearchResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(App.currentActivity, VocabDescriptionActivity.class);
+                intent.putExtra("VocabularyId", vocabulary.getId());
+                App.currentActivity.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return vocabularies.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtSearchResult;
+
+        public ViewHolder(View view) {
+            super(view);
+            txtSearchResult = (TextView) view.findViewById(R.id.txtSearchResult);
+        }
+    }
+}
