af0cdb249f39b0befb111671abc04c95a5b3e5a6
ali <ali.faghani@gmail.com>
2015-09-22 14:32:57 +0330

    System settings

--- app/src/main/java/com/zohaltech/app/corevocabulary/activities/MainActivity.java
+++ app/src/main/java/com/zohaltech/app/corevocabulary/activities/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends EnhancedActivity {
         setContentView(R.layout.activity_main);
         startTime = System.currentTimeMillis() - 5000;
 
-        if (!App.preferences.getBoolean("Encoded", false))
+        if (App.preferences.getBoolean("Encoded", false))
             EncryptVocabs();
     }
 
