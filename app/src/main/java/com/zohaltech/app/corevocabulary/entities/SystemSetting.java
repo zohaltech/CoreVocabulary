2ef96365478defa3b48c31a4c5cbb16f5e8e5165
ostademast <ostademast@yahoo.com>
2015-09-27 18:16:28 +0330

    payment added

--- app/src/main/java/com/zohaltech/app/corevocabulary/entities/SystemSetting.java
+++ app/src/main/java/com/zohaltech/app/corevocabulary/entities/SystemSetting.java
@@ -53,6 +53,7 @@ public class SystemSetting {
     }
 
     public boolean isPremium(){
+        // TODO: 2015/09/27
         return true;
         //return getPremiumVersion() != null && getPremiumVersion().equals(Helper.hashString(Helper.getDeviceId()));
     }
