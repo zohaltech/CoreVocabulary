481a4a33a3a37f76070226f15ad54471d81a048f
ostademast <ostademast@yahoo.com>
2015-10-17 18:24:51 +0330

    fix some bugs for notification

--- app/src/main/java/com/zohaltech/app/corevocabulary/classes/AlarmReceiver.java
+++ app/src/main/java/com/zohaltech/app/corevocabulary/classes/AlarmReceiver.java
@@ -38,22 +38,22 @@ public class AlarmReceiver extends BroadcastReceiver {
         resultIntent.putExtra(VocabularyDetailsActivity.VOCAB_ID, reminder.getVocabularyId());
 
 
-        // Creates an explicit intent for an Activity in your app
-        //Intent resultIntent = new Intent(this, ResultActivity.class);
+        //// Creates an explicit intent for an Activity in your app
+        ////Intent resultIntent = new Intent(this, ResultActivity.class);
+        //
+        //// The stack builder object will contain an artificial back stack for the
+        //// started Activity.
+        //// This ensures that navigating backward from the Activity leads out of
+        //// your application to the Home screen.
+        //TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        //// Adds the back stack for the Intent (but not the Intent itself)
+        ////stackBuilder.addParentStack(ResultActivity.class);
+        //// Adds the Intent that starts the Activity to the top of the stack
+        //stackBuilder.addNextIntent(resultIntent);
+        //PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        // Adds the back stack for the Intent (but not the Intent itself)
-        //stackBuilder.addParentStack(ResultActivity.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
-
-        //PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(resultPendingIntent);
 
 
